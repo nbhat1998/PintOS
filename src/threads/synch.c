688d0b338b6922a162cdb9d2a59dfffe936bf18c
gs2617 <gs2617@ic.ac.uk>
2018-10-20 16:20:31 +0100

    Moved function implementation list filter

--- src/threads/synch.c
+++ src/threads/synch.c
@@ -218,16 +218,10 @@ bool lock_try_acquire(struct lock *lock)
   return success;
 }
 
-
 /* Removes all list elements that contain the lock
    passed to the function */
-void donation_list_filter (struct list * list, struct lock * lock) {
-  for(struct list_elem curr = list->head; &curr != &list->tail; curr = *curr.next) {
-    if (&list_entry(&curr, struct donation, elem)->lock == lock) {
-      list_remove(&curr);
-    }
-  }
-}
+void donation_list_filter(struct list *list, struct lock *lock);
+
 
 /* Releases LOCK, which must be owned by the current thread.
 
@@ -239,13 +233,16 @@ void lock_release(struct lock *lock)
   ASSERT(lock != NULL);
   ASSERT(lock_held_by_current_thread(lock));
 
-  struct thread* me = thread_current();
+  struct thread *me = thread_current();
 
-  list_filter(&me->don_list, lock);
-  
-  if (list_size(&me->don_list) == 0) {
+  donation_list_filter(&me->don_list, lock);
+
+  if (list_size(&me->don_list) == 0)
+  {
     thread_set_priority(me->init_priority);
-  } else {
+  }
+  else
+  {
     thread_set_priority(list_entry(list_begin(&me->don_list), struct donation, elem)->priority);
   }
 
@@ -349,4 +346,15 @@ void cond_broadcast(struct condition *cond, struct lock *lock)
 
   while (!list_empty(&cond->waiters))
     cond_signal(cond, lock);
+}
+
+void donation_list_filter(struct list *list, struct lock *lock)
+{
+  for (struct list_elem curr = list->head; &curr != &list->tail; curr = *curr.next)
+  {
+    if (list_entry(&curr, struct donation, elem)->lock == lock)
+    {
+      list_remove(&curr);
+    }
+  }
 }
