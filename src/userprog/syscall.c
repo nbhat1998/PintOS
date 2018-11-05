73ad2a73b0db446ffa36544a8919f071a9d52c04
Maria Teguiani <maria.teguiani17@imperial.ac.uk>
2018-11-05 16:16:07 +0000

    Implemented system call function pointers and function skeletons

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -4,17 +4,102 @@
 #include "threads/interrupt.h"
 #include "threads/thread.h"
 
-static void syscall_handler (struct intr_frame *);
+static void syscall_handler(struct intr_frame *);
 
-void
-syscall_init (void) 
+void sys_halt(uint32_t *args);
+void sys_exit(uint32_t *args);
+void sys_exec(uint32_t *args);
+void sys_wait(uint32_t *args);
+void sys_remove(uint32_t *args);
+void sys_open(uint32_t *args);
+void sys_filesize(uint32_t *args);
+void sys_tell(uint32_t *args);
+void sys_close(uint32_t *args);
+void sys_create(uint32_t *args);
+void sys_seek(uint32_t *args);
+void sys_read(uint32_t *args);
+void sys_write(uint32_t *args);
+
+void(*syscalls[13]) = {
+    sys_halt,
+    sys_exit,
+    sys_exec,
+    sys_wait,
+    sys_remove,
+    sys_open,
+    sys_filesize,
+    sys_tell,
+    sys_close,
+    sys_create,
+    sys_seek,
+    sys_read,
+    sys_write
+  };
+
+void syscall_init(void)
 {
-  intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
+  intr_register_int(0x30, 3, INTR_ON, syscall_handler, "syscall");
 }
 
 static void
-syscall_handler (struct intr_frame *f UNUSED) 
+syscall_handler(struct intr_frame *f)
 {
-  printf ("system call!\n");
-  thread_exit ();
+  printf("system call!\n");
+  uint32_t *args = f->esp + 1;
+  (*syscalls[*f->esp])(args);
+
+  //thread_exit(); //?
+}
+
+void sys_halt(uint32_t *args)
+{
+
+}
+
+void sys_exit(uint32_t *args) {
+
+}
+
+void sys_exec(uint32_t *args) {
+
+}
+
+void sys_wait(uint32_t *args) {
+
+}
+
+void sys_remove(uint32_t *args) {
+
+}
+
+void sys_open(uint32_t *args) {
+
+}
+
+void sys_filesize(uint32_t *args) {
+
+}
+
+void sys_tell(uint32_t *args) {
+
+}
+
+void sys_close(uint32_t *args) {
+
+}
+
+void sys_create(uint32_t *args) {
+
+}
+
+void sys_seek(uint32_t *args) {
+
+}
+
+void sys_read(uint32_t *args) {
+
+}
+
+void sys_write(uint32_t *args) {
+
 }
