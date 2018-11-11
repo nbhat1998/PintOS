7a68c63d655ea128be941576e6de4921fcfcb486
gs2617 <gs2617@ic.ac.uk>
2018-11-11 21:15:52 +0000

    Fixed syscall write, implemented output to terminal when fd = 1

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -6,7 +6,6 @@
 #include "threads/vaddr.h"
 #include "../filesys/filesys.h"
 
-
 /* Reads a byte at user virtual address UADDR.
 UADDR must be below PHYS_BASE.
 Returns the byte value if successful, -1 if a segfault
@@ -23,19 +22,19 @@ get_user(const uint8_t *uaddr)
 
 #define WORD_LENGTH 4
 #define BYTE 8
-static int32_t 
-get_word(uint8_t *uaddr) 
+static int32_t
+get_word(uint8_t *uaddr)
 {
   int32_t word = 0;
 
-  for(int i = 0; i < WORD_LENGTH; i++) 
+  for (int i = 0; i < WORD_LENGTH; i++)
   {
     int new_byte = 0;
     new_byte = get_user(uaddr);
     if (new_byte == -1)
     {
       PANIC("new_byte is -1, panic!");
-      // TODO: check if we really need the panic, "very sure we need the if statement" - iulia 
+      // TODO: check if we really need the panic, "very sure we need the if statement" - iulia
     }
     new_byte <<= i * BYTE;
     word += new_byte;
@@ -60,7 +59,6 @@ put_user(uint8_t *udst, uint8_t byte)
 
 /* Lock used by filesystem */
 // TODO: LOCK INIT SOMEWHERE...
-
 
 static void syscall_handler(struct intr_frame *);
 
@@ -105,7 +103,7 @@ allocate_fd(void)
   int fd;
 
   // TODO: USE A DIFFERENT LOCK!!!!!! (global)
-  // TODO: Perform lock_init on this lock at some point  
+  // TODO: Perform lock_init on this lock at some point
   lock_acquire(&filesys_lock);
   fd = next_fd++;
   lock_release(&filesys_lock);
@@ -113,26 +111,25 @@ allocate_fd(void)
   return fd;
 }
 
-
 static void
 syscall_handler(struct intr_frame *f)
 {
-  printf("system call!\n");
+  //printf("system call!\n");
   int function = get_word(f->esp);
-  if(function == -1) {
+  if (function == -1)
+  {
     // TODO: DO SOMETHING BAD
   }
 
-
   uint32_t *args = (uint32_t *)f->esp + 1;
-  hex_dump(0, args, (int8_t *) PHYS_BASE - ((int8_t*) args), true);
+  //hex_dump(0, args, (int8_t *)PHYS_BASE - ((int8_t *)args), true);
 
   f->eax = syscalls[function](args);
 }
 
 uint32_t sys_halt(uint32_t *args)
 {
-  shutdown_power_off(); 
+  shutdown_power_off();
   // good night sweet prince
   NOT_REACHED();
   return 0;
@@ -140,7 +137,7 @@ uint32_t sys_halt(uint32_t *args)
 
 uint32_t sys_exit(uint32_t *args)
 {
-  // thread_current()->process->status = 
+  // thread_current()->process->status =
   thread_exit();
   NOT_REACHED();
   return 0;
@@ -159,43 +156,41 @@ uint32_t sys_wait(uint32_t *args)
 uint32_t sys_create(uint32_t *args)
 {
   char *file = get_word(args);
-  char *file_name; 
-  strlcpy(file_name,file,sizeof(file));
+  char *file_name;
+  strlcpy(file_name, file, sizeof(file));
 
-  uint8_t* char_pointer = (uint8_t*) args;
+  uint8_t *char_pointer = (uint8_t *)args;
   char_pointer += sizeof(file_name);
-  args = (uint32_t*) char_pointer;
+  args = (uint32_t *)char_pointer;
 
-  unsigned initial_size = get_word(args); 
+  unsigned initial_size = get_word(args);
 
   lock_acquire(&filesys_lock);
-  bool success = filesys_create(file_name,initial_size);
+  bool success = filesys_create(file_name, initial_size);
   lock_release(&filesys_lock);
 
-  return success; 
+  return success;
   // TODO: dirty casting going on here, reconsider during design decisions?
-
 }
 
 uint32_t sys_remove(uint32_t *args)
 {
-  char *file = get_word(args); 
-  char *file_name; 
-  strlcpy(file_name,file,sizeof(file)); 
-  bool success_value; 
+  char *file = get_word(args);
+  char *file_name;
+  strlcpy(file_name, file, sizeof(file));
+  bool success_value;
 
-  // TODO : to be continued after desgn decision on whether or not a file should keep track of all the processes who refer to it, so that if a file has been closed and then the all processes which have file descriptors for it are closed, then the file should cease to exist 
-  
+  // TODO : to be continued after desgn decision on whether or not a file should keep track of all the processes who refer to it, so that if a file has been closed and then the all processes which have file descriptors for it are closed, then the file should cease to exist
 }
 
 uint32_t sys_open(uint32_t *args)
 {
-  char* file = get_word(args);
-  char* file_name;
+  char *file = get_word(args);
+  char *file_name;
   strlcpy(file_name, file, sizeof(file));
-  struct file_container* new_file = malloc(sizeof (struct file_container));
+  struct file_container *new_file = malloc(sizeof(struct file_container));
   new_file->fd = allocate_fd();
-  
+
   lock_acquire(&filesys_lock);
   new_file->f = filesys_open(file_name);
   list_push_back(&thread_current()->process->file_containers, &new_file->elem);
@@ -207,58 +202,58 @@ uint32_t sys_open(uint32_t *args)
 uint32_t sys_filesize(uint32_t *args)
 {
   int param_fd = (int)get_word(args);
-  int length_of_file;  
-  for (struct list_elem* e = list_begin(&thread_current()->process->file_containers); e != list_end(&thread_current()->process->file_containers); e = list_next(e))
+  int length_of_file;
+  for (struct list_elem *e = list_begin(&thread_current()->process->file_containers); e != list_end(&thread_current()->process->file_containers); e = list_next(e))
   {
     struct file_container *this_container = list_entry(e, struct file_container, elem);
     if (param_fd == this_container->fd)
     {
-      lock_acquire(&filesys_lock); 
-      length_of_file = file_length(this_container->f); 
-      lock_release(&filesys_lock); 
+      lock_acquire(&filesys_lock);
+      length_of_file = file_length(this_container->f);
+      lock_release(&filesys_lock);
       break;
     }
   }
-  return length_of_file;  
+  return length_of_file;
 }
 
 uint32_t sys_seek(uint32_t *args)
 {
-  int param_fd = (int)get_word(args); 
-  args = args + 1 ; 
+  int param_fd = (int)get_word(args);
+  args = args + 1;
 
   unsigned param_position = (unsigned)get_word(args);
 
-  for ( struct list_elem *e = list_begin(&thread_current()->process->file_containers); e != list_end(&thread_current()->process->file_containers); e = list_next(e))
+  for (struct list_elem *e = list_begin(&thread_current()->process->file_containers); e != list_end(&thread_current()->process->file_containers); e = list_next(e))
   {
-    struct file_container *this_container = list_entry(e,struct file_container, elem);
+    struct file_container *this_container = list_entry(e, struct file_container, elem);
     if (param_fd == this_container->fd)
     {
-      lock_acquire(&filesys_lock); 
+      lock_acquire(&filesys_lock);
 
-      file_seek(this_container->f,param_position);
+      file_seek(this_container->f, param_position);
       lock_release(&filesys_lock);
-      break; 
+      break;
     }
   }
 
-  return; 
+  return;
 }
 
 uint32_t sys_tell(uint32_t *args)
 {
   int param_fd = (int)get_word(args);
 
-  unsigned tell_value; 
-  for (struct list_elem* e = list_begin(&thread_current()->process->file_containers); e != list_end(&thread_current()->process->file_containers); e = list_next(e))
+  unsigned tell_value;
+  for (struct list_elem *e = list_begin(&thread_current()->process->file_containers); e != list_end(&thread_current()->process->file_containers); e = list_next(e))
   {
-    struct file_container *this_container = list_entry(e,struct file_container, elem);
+    struct file_container *this_container = list_entry(e, struct file_container, elem);
     if (param_fd == this_container->fd)
     {
       lock_acquire(&filesys_lock);
-      tell_value = file_tell(this_container->f); 
-      lock_release(&filesys_lock); 
-      break; 
+      tell_value = file_tell(this_container->f);
+      lock_release(&filesys_lock);
+      break;
     }
   }
 
@@ -274,42 +269,51 @@ uint32_t sys_read(uint32_t *args)
 uint32_t sys_write(uint32_t *args)
 {
   int param_fd = (int)get_word(args);
-  args = args + 1; 
+  args = args + 1;
 
-  void *param_buffer = get_word(args); 
+  char *param_buffer = (char *)get_word(args);
+  //char *param_buffer_kernel;
+  //strlcpy(param_buffer_kernel, param_buffer, sizeof(param_buffer));
 
-
-  uint8_t* void_pointer = (uint8_t*) args;
+  uint8_t *void_pointer = (uint8_t *)args;
   void_pointer += sizeof(param_buffer);
-  args = (uint32_t*) void_pointer;
+  args = (uint32_t *)void_pointer;
 
-  unsigned param_size = (unsigned)get_word(args); 
-  int32_t actually_written = 0; 
-  for (struct list_elem* e = list_begin(&thread_current()->process->file_containers); e != list_end(&thread_current()->process->file_containers); e = list_next(e))
+  unsigned param_size = (unsigned)get_word(args);
+  int32_t actually_written = 0;
+
+  if (param_fd == 1)
   {
-    struct file_container *this_container = list_entry(e,struct file_container, elem);
-    if (param_fd == this_container->fd)
+    //printf("size: %d\n", strlen(param_buffer));
+    if (strlen(param_buffer) < 500)
     {
-      lock_acquire(&filesys_lock);
-      actually_written = file_write(this_container->f,param_buffer,param_size);
-      lock_release(&filesys_lock);
-      break;
+      putbuf(param_buffer, strlen(param_buffer));
+      return strlen(param_buffer);
     }
   }
-
-  return actually_written; 
-
+  else
+  {
+    lock_acquire(&filesys_lock);
+    for (struct list_elem *e = list_begin(&thread_current()->process->file_containers); e != list_end(&thread_current()->process->file_containers); e = list_next(e))
+    {
+      struct file_container *this_container = list_entry(e, struct file_container, elem);
+      if (param_fd == this_container->fd)
+      {
+        actually_written = file_write(this_container->f, param_buffer, param_size);
+        break;
+      }
+    }
+    lock_release(&filesys_lock);
+    return actually_written;
+  }
 }
-
-
-
 
 uint32_t sys_close(uint32_t *args)
 {
-  int param_fd = (int)get_word(args); 
-  for (struct list_elem* e = list_begin(&thread_current()->process->file_containers); e != list_end(&thread_current()->process->file_containers); e = list_next(e))
+  int param_fd = (int)get_word(args);
+  for (struct list_elem *e = list_begin(&thread_current()->process->file_containers); e != list_end(&thread_current()->process->file_containers); e = list_next(e))
   {
-    struct file_container *this_container = list_entry(e,struct file_container, elem);
+    struct file_container *this_container = list_entry(e, struct file_container, elem);
     if (param_fd == this_container->fd)
     {
       lock_acquire(&filesys_lock);
@@ -317,7 +321,6 @@ uint32_t sys_close(uint32_t *args)
       lock_release(&filesys_lock);
     }
     // TODO: Exiting or terminating process implicitly closes all open file descriptors
-    return; 
-      
+    return;
   }
 }
