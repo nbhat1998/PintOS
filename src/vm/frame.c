13dbd469669cd8585f2726f87044a6d296795f52
nb1317 <nb1317@ic.ac.uk>
2018-11-28 21:41:26 +0000

    Added remove frames function nb1317 mt5217 gs2617

--- src/vm/frame.c
+++ src/vm/frame.c
@@ -1,6 +1,7 @@
 #include "vm/frame.h"
 #include "threads/malloc.h"
 #include "threads/thread.h"
+#include "userprog/pagedir.h"
 
 void create_frame(uint32_t *physical_memory_key, uint32_t *uaddr)
 {
@@ -20,7 +21,6 @@ void create_frame(uint32_t *physical_memory_key, uint32_t *uaddr)
     }
   }
 
-
   struct frame *new_frame = malloc(sizeof(struct frame));
   new_frame->physical_memory_key = physical_memory_key;
   list_push_back(&new_frame->user_ptes, new_pte_ptr);
@@ -30,7 +30,37 @@ void create_frame(uint32_t *physical_memory_key, uint32_t *uaddr)
 
 void remove_frames(uint32_t *physical_memory_key)
 {
-  // TODO: iterate through the list, find every frame and
-  // remove from pagedir entries and from frame_table list
-  // then free
+  struct list_elem *e = list_begin(&frame_table);
+
+  while (e != list_end(&frame_table))
+  {
+    struct frame *curr = list_entry(e, struct frame, elem);
+
+    if (curr->physical_memory_key == physical_memory_key)
+    {
+      struct list_elem *f = list_begin(&curr->user_ptes);
+
+      while (f != list_end(&curr->user_ptes))
+      {
+        struct user_pte_ptr *current = list_entry(f, struct user_pte_ptr, elem);
+        uint32_t *pte = get_pte(current->pagedir, current->uaddr, false);
+        if (pte != NULL)
+        {
+          /* Set the present bit (bit 0) to 0 */
+          pte = (uint32_t)pte & ((1 << 32) - 2);
+        }
+
+        struct list_elem *temp = f;
+        f = list_next(f);
+        list_remove(temp);
+        free(current);
+      }
+
+      list_remove(e);
+      free(curr);
+      return;
+    }
+    
+    e = list_next(e);
+  }
 }
